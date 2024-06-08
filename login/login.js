268d9c5070156a393db352523c264a50d72d5319
Thien <nguyenlehuuthien1808@gmail.com>
2024-06-08 21:05:07 +0700

    Update trang chu, dang ki, dang nhap, navbar, footer

--- login/login.js
+++ login/login.js
@@ -6,26 +6,14 @@ import {
   signInWithEmailAndPassword,
 } from "../firebase.js";
 
-const login = document.getElementById("login");
+const login = document.getElementById("Login");
 login.addEventListener("submit", (event) => {
   event.preventDefault();
   let email = document.getElementById("exampleInputEmail1").value;
   let pw = document.getElementById("exampleInputPassword1").value;
-  const gmailRegex = /^[a-zA-Z0-9]+@gmail.com$/;
-  const emailValue = email.trim();
 
   if (email.length == 0 || pw.length == 0) {
     alert("Please check your username and gmail");
-  } else if (!gmailRegex.test(emailValue)) {
-    alert("Please check your gmail");
-  } else if (
-    //!pw.value.trim().match()lowerCase//
-    pw.value.trim().length !== 8 ||
-    !/[A-Z]/.test(pw.value) || //Kiểm tra chữ hoa, chữ thường, số//
-    !/[a-z]/.test(pw.value) ||
-    !/[0-9]/.test(pw.value)
-  ) {
-    alert("Please check your password");
   } else {
     signInWithEmailAndPassword(auth, email, pw)
       .then((userCredential) => {
