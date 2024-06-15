87c2e0672a3144a81783b5616f668b9b0be2191e
Thien <nguyenlehuuthien1808@gmail.com>
2024-06-15 21:06:31 +0700

    Them product, about us

--- main/index.js
+++ main/index.js
@@ -30,3 +30,50 @@ btnLogout.addEventListener("click", () => {
       console.error(error);
     });
 });
+
+const products = [
+  {
+    id: 0,
+    name: "JAVA MONSTER MEAN BEAN",
+    img: "../image/bean.webp",
+  },
+  {
+    id: 1,
+
+    img: "../image/ori.webp",
+  },
+  {
+    id: 2,
+
+    img: "../image/ultraa.webp",
+  },
+  {
+    id: 3,
+
+    img: "../image/juicee.webp",
+  },
+  {
+    id: 4,
+
+    img: "../image/zerosu.webp",
+  },
+  {
+    id: 5,
+
+    img: "../image/tea.webp",
+  },
+];
+const productsItem = document.getElementById("getProduct");
+console.log(productsItem);
+
+function getProduct() {
+  for (let i = 0; i < products.length; i++) {
+    productsItem.innerHTML += ` 
+    <div class="product" class="card row col-md8"  style="width: 10rem;" >
+      <img class="image" src="${products[i].img}" class="card-img-top" alt="...">
+      <div class="card-body">
+      
+    </div>`;
+  }
+}
+getProduct();
